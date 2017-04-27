ace81695a3a7b2d62f766ff96dca065e8f4dccee
Vladimir Zahajsky <vladimir.zahajsky@mall.cz>
2017-04-27 14:38:05 +0200

    pripraveno fixovani komitovanejch souboru, zhezceny hlasky, upravene readme

--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
-Coding Standard 101
-====================
-***
+# Coding Standard 101
+
 ### Easy way to implement coding standards to your project
 ## About
 Easy
@@ -22,38 +21,49 @@ $ composer require sercode/coding-standard-101 --dev
 
 ## Basic usage
 
-Run with Php_CodeSniffer:
+Run with Code sniffer for check code:
 
 ```sh
 vendor/bin/phpcs src --standard=vendor/sercode/coding-standard-101/src/ruleset.xml -p
 ```
 
-That's all! This check all files in your project against default `ruleset.xml`
+Run this for fix all errors in project:
+````sh
+vendor/bin/phpcbf src --standard=vendor/sercode/coding-standard-101/src/ruleset.xml -p
+````
 
+That's all for really base usage! This check all files in your project against default `ruleset.xml`
 
-## How to be both Lazy and Safe
+## How to be safe
 
-### Composer hook
+### Checking only files in commit
 
 In case you don't want to use Php_CodeSniffer manually for every change in the code you make, you can add pre-commit hook via `composer.json`.
 **Every time you try to commit, Php_CodeSniffer will run on changed `.php` files for actual commit only.**
 
-##### 1) If you want do something
+##### 1) Manual usage
+
+
 ```json
 "scripts": {
     "cs-install-prehook": [
 		"SerCode\\CodingStandard101\\Composer\\ScriptHandler::addPhpCsToPreCommitHook"
 	]
 }		
-````	
+```
 
-After you run **composer update** or **composer install** just run: 
+After you run **composer update** or **composer install** just run:
+ 
 ```
 composer cs-install-prehook
 ```
+**For proper usage all features zou have to run this script**
+
+
 *If you don't change path to `ruleset.xml` (see bellow), you don't need to run this script again*
 
 ##### 2) if you are lazy or just be sure
+Just add some code:
 
 ```json
 "scripts": {
@@ -77,7 +87,22 @@ This secure install git commit prehook every time when you run  **composer updat
 **If you want commit without running codesniffer, just add `--no-verify` to commit command**:
 ```
 git commit -m "TEST" --no-verify
-````
+```
+### Fixing only files in commit
+To `composer.json` put:
+
+```json
+"scripts": {
+  "fix-cs-commit": [
+    "SerCode\\CodingStandard101\\Composer\\ScriptHandler::fixCsCommit"
+  ]
+}
+```
+and run
+ ```
+ composer fix-cs-commit
+ ```
+
 ***
 ## Advanced usage
 
--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -9,49 +9,54 @@ final class ScriptHandler
 	const RULESET = 'ruleset.xml';
 
 	const INI_NAME = '.csStandard';
-	
+
 	const BASH_START = '#!/bin/sh';
 	const PREHOOK_START = '### coding-standard-101 -start';
 	const PREHOOK_END = '### coding-standard-101 -end';
-	
+	const SH_SCRIPTS_FOLDER = 'shScripts';
+	const FIX_CS_COMMIT_SCRIPT = 'fixCsCommit.sh';
+
 	/**
 	 * inserts prehook
 	 * @return type
 	 */
 	public static function addPhpCsToPreCommitHook()
 	{
-		$originFile = getcwd() . '/.git/hooks/pre-commit';
+		$originPrecommitFile = getcwd() . '/.git/hooks/pre-commit';
 
-		if(!file_exists(dirname($originFile))){
-			echo 'Creating non-existent folders ...' .PHP_EOL;
-			mkdir(dirname($originFile), 0777, TRUE);
+		if (!file_exists(dirname($originPrecommitFile))) {
+			echo 'Creating non-existent folders ...' . PHP_EOL;
+			mkdir(dirname($originPrecommitFile), 0777, TRUE);
 		}
 
 		$ini = self::setupIni();
+		$rulesetPath = $ini['ruleset'][0];
+		self::createFixCsCommitScript($rulesetPath);
 
-		if (!isset($ini['ruleset']) || count($ini['ruleset'])==0){
+
+		if (!isset($ini['ruleset']) || count($ini['ruleset']) == 0) {
 			echo 'ERROR: Missing path to ' . self::RULESET . PHP_EOL;
 			return -1;
 		}
 
 		$templateContent = file_get_contents(__DIR__ . '/templates/git/hooks/pre-commit-phpcs');
-		$templateContent = sprintf($templateContent, $ini['ruleset'][0]);
-		
-		if (file_exists($originFile)) {
-			$originContent = file_get_contents($originFile);
+		$templateContent = sprintf($templateContent, $rulesetPath);
+
+		if (file_exists($originPrecommitFile)) {
+			$originContent = file_get_contents($originPrecommitFile);
 			if (strpos($originContent, self::PREHOOK_START) === FALSE) {
 				echo 'Inserting pre-comit hook ...' . PHP_EOL;
 				$newContent = $originContent . PHP_EOL . PHP_EOL . self::BASH_START . PHP_EOL . PHP_EOL . $templateContent;
-				file_put_contents($originFile, $newContent);
+				file_put_contents($originPrecommitFile, $newContent);
 			} else {
 				echo 'Prehook already exist.' . PHP_EOL;
-				self::updateContent($originFile, $templateContent);
+				self::updateContent($originPrecommitFile, $templateContent);
 			}
 		} else {
 			echo 'Creating file and inserting pre-comit hook ...' . PHP_EOL;
-			file_put_contents($originFile,self::BASH_START . PHP_EOL . PHP_EOL .$templateContent);
+			file_put_contents($originPrecommitFile, self::BASH_START . PHP_EOL . PHP_EOL . $templateContent);
 		}
-		chmod($originFile, 0755);
+		chmod($originPrecommitFile, 0755);
 	}
 
 	/**
@@ -59,7 +64,8 @@ final class ScriptHandler
 	 * @param type $event
 	 * @return boolean
 	 */
-	public static function installCodeSniffStandards($event) {
+	public static function installCodeSniffStandards($event)
+	{
 
 		$workDir = getcwd();
 
@@ -67,34 +73,34 @@ final class ScriptHandler
 
 		$standardsPaths = [];
 
-		if (isset($ini['packages']) && count($ini['packages'])>0) {
+		if (isset($ini['packages']) && count($ini['packages']) > 0) {
 			$packages = $ini['packages'];
 
-			foreach($packages as $package){
+			foreach ($packages as $package) {
 				$pathDir = $workDir . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . strtolower(str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $package));
-				if (is_dir($pathDir)){
+				if (is_dir($pathDir)) {
 					$pathRuleset = self::justFindFile($pathDir);
 					if ($pathRuleset) {
 						$standardsPaths[$package] = $pathRuleset;
 					} else {
-						echo 'WARNING: Non-existent "'.self::RULESET.'" in package "'.$package.'" => skipping => add file' . PHP_EOL ;
+						echo 'WARNING: Non-existent "' . self::RULESET . '" in package "' . $package . '" => skipping => add file' . PHP_EOL;
 					}
 				} else {
-					echo 'WARNING: Non-existent folder with package "'.$package.'" => skipping => wrong package name?' . PHP_EOL ;
+					echo 'WARNING: Non-existent folder with package "' . $package . '" => skipping => wrong package name?' . PHP_EOL;
 				}
 			}
 
-			if (!empty($standardsPaths)){
+			if (!empty($standardsPaths)) {
 				$newSourceString = join(',', $standardsPaths);
 			} else {
-				echo PHP_EOL.'WARNING: None package is valid' .PHP_EOL;
+				echo PHP_EOL . 'WARNING: None package is valid' . PHP_EOL;
 				return FALSE;
 			}
 
-			echo PHP_EOL.'Setting up installed standards for Code Sniffer to ' . $newSourceString .PHP_EOL;
-			echo shell_exec('phpcs --config-set installed_paths '. $newSourceString);
+			echo PHP_EOL . 'Setting up installed standards for Code Sniffer to ' . $newSourceString . PHP_EOL;
+			echo shell_exec('phpcs --config-set installed_paths ' . $newSourceString);
 		} else {
-			echo PHP_EOL.'None packages for register.' .PHP_EOL;
+			echo PHP_EOL . 'None packages for register.' . PHP_EOL;
 		}
 
 	}
@@ -104,12 +110,13 @@ final class ScriptHandler
 	 * @param type $searchDir
 	 * @return boolean
 	 */
-	public static function justFindFile($searchDir){
+	public static function justFindFile($searchDir)
+	{
 		$it = new \RecursiveDirectoryIterator($searchDir, \RecursiveDirectoryIterator::SKIP_DOTS);
 		foreach (new \RecursiveIteratorIterator($it) as $file) {
 			$pathParts = explode(DIRECTORY_SEPARATOR, $file);
 			$fileName = array_pop($pathParts);
-			if ( $fileName === self::RULESET) {
+			if ($fileName === self::RULESET) {
 				return dirname(dirname($file));
 			}
 		}
@@ -120,7 +127,8 @@ final class ScriptHandler
 	 * get ini and merge with default
 	 * @return type
 	 */
-	public static function setupIni(){
+	public static function setupIni()
+	{
 		$workDir = getcwd();
 
 		$defaultIniPath = __DIR__ . '/../' . self::INI_NAME;
@@ -128,26 +136,27 @@ final class ScriptHandler
 
 		$defaultIni = parse_ini_file($defaultIniPath, TRUE);
 
-		if (file_exists($overrideIniPath)){
+		if (file_exists($overrideIniPath)) {
 			$ownIni = parse_ini_file($overrideIniPath, TRUE);
 		}
 
-		if (isset($ownIni)){
+		if (isset($ownIni)) {
 			$ini = array_merge_recursive($ownIni, $defaultIni);
 			$ini['packages'] = array_unique($ini['packages']);
 		} else {
 			$ini = $defaultIni;
 		}
-		
+
 		return $ini;
 	}
-	
+
 	/**
 	 * finde and replace cs101 part in prehook file
 	 * @param type $originFile
 	 * @param type $templateContent
 	 */
-	public static function updateContent($originFile, $templateContent){
+	public static function updateContent($originFile, $templateContent)
+	{
 		$file = file($originFile);
 
 		// need improvement
@@ -156,29 +165,29 @@ final class ScriptHandler
 		$restartInclude = false;
 		$hasStart = false;
 		$hasEnd = false;
-		foreach($file as $index => $line){
-			if ($index == 0 && !(trim($line)== self::BASH_START)){
-				$out[] = self::BASH_START .PHP_EOL .PHP_EOL;
+		foreach ($file as $index => $line) {
+			if ($index == 0 && !(trim($line) == self::BASH_START)) {
+				$out[] = self::BASH_START . PHP_EOL . PHP_EOL;
 			}
 
-			if (trim($line)== self::PREHOOK_START){
+			if (trim($line) == self::PREHOOK_START) {
 				$include = false;
 				$hasStart = true;
-			} elseif (trim($line)== self::PREHOOK_END){
+			} elseif (trim($line) == self::PREHOOK_END) {
 				$restartInclude = true;
 				$hasEnd = true;
 			}
-			if ($include === true){
+			if ($include === true) {
 				$out[] = $line;
 			}
 
-			if ($restartInclude === true){
+			if ($restartInclude === true) {
 				$include = true;
 				$out[] = $templateContent;
 			}
 		}
 
-		if (!$hasEnd || !$hasStart){
+		if (!$hasEnd || !$hasStart) {
 			echo 'ERROR: Precommit hook file missing start or end part line: ' . self::PREHOOK_END . ' => file left untouched.' . PHP_EOL;
 			exit;
 		}
@@ -186,11 +195,45 @@ final class ScriptHandler
 		echo 'Renewing cs101 prehook part...' . PHP_EOL;
 		$fp = fopen($originFile, "w+");
 		flock($fp, LOCK_EX);
-		foreach($out as $line) {
-				fwrite($fp, $line);
+		foreach ($out as $line) {
+			fwrite($fp, $line);
 		}
 		flock($fp, LOCK_UN);
-		fclose($fp); 
-		
+		fclose($fp);
+
 	}
+
+	/**
+	 * prepare fix commit script
+	 * @param $rulesetPath
+	 */
+	public static function createFixCsCommitScript($rulesetPath)
+	{
+		$pathToScript = __DIR__ . DIRECTORY_SEPARATOR . self::SH_SCRIPTS_FOLDER . DIRECTORY_SEPARATOR . self::FIX_CS_COMMIT_SCRIPT;
+
+		$path = __DIR__ . DIRECTORY_SEPARATOR . self::SH_SCRIPTS_FOLDER;
+		/*var_dump($path);
+		var_dump(is_dir($path));
+//		var_dump(__DIR__);
+		var_dump($pathToScript);
+//		var_dump(is_dir(DIRECTORY_SEPARATOR . self::SH_SCRIPTS_FOLDER));
+		var_dump(file_exists($pathToScript));*/
+		$templateContent = file_get_contents(__DIR__ . '/templates/phpcbf/fix-commit-file');
+		$templateContent = sprintf($templateContent, $rulesetPath);
+
+		file_put_contents($pathToScript, $templateContent);
+		chmod($pathToScript, 0755);
+	}
+
+
+	/**
+	 * run script for fix commit files
+	 */
+	public static function fixCsCommit()
+	{
+		$a = [];
+		exec('sh vendor/sercode/coding-standard-101/src/Composer/shScripts/fixCsCommit.sh', $a);
+		echo implode("\n", $a);
+	}
+
 }
